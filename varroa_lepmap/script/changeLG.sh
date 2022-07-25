f4e383156d9acc23b84997def33797338e2eb9bd
Nurit Eliash <nurit.eliash@oist.jp>
2022-07-25 16:56:18 +0900

    change marker's LG
    make a RMD to chamge the mark'es LG based on the main LG in each chromosome

---
+++ varroa_lepmap/script/changeLG.sh
@@ -0,0 +1,17 @@
+# Code's aim: changing marker's LG to the main LG in the chromosome
+
+# state the main LG in each chromosome:
+
+# Chr          largest LG
+NW_019211454.1 13
+NW_019211455.1 16
+NW_019211456.1 2
+NW_019211457.1 6
+NW_019211458.1 3
+NW_019211459.1 1
+NW_019211460.1 11
+
+
+# make a file with the columns: marker's order (1:16986), and its LG (1-7, based on the above table)
+
+# 
